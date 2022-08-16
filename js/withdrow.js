045ea69ac09502af5a06fc3ef83546b25f249f39
swadebsharma6 <swadebriva2012@gmail.com>
2022-08-16 08:20:54 +0600

    added withdrow file

---
+++ js/withdrow.js
@@ -0,0 +1,33 @@
+// step:01 add event listener to the deposit button
+
+document.getElementById('btn-withdrow').addEventListener('click', function(){
+    
+
+    //step:02 get the amount from withdro input field.
+    const withdrowField = document.getElementById('withdrow-field');
+    const newWithdrowAmountString = withdrowField.value ;
+    const newWithdrowAmount = parseFloat (newWithdrowAmountString);
+    // console.log(newWithdrowAmount);
+
+    // step:03 get the withdrow ammount withdrow input field
+    const withdrowTotalElement = document.getElementById('withdrow-total');
+    const previousWithdrowTotalString = withdrowTotalElement.innerText;
+    const previousWithdrowTotal = parseFloat(previousWithdrowTotalString);
+
+    // step: 04 calculate total withdrow ammount.
+    const currentWithdrowTotal = newWithdrowAmount + previousWithdrowTotal;
+    withdrowTotalElement.innerText = currentWithdrowTotal;
+
+    // step: 05 get the previous balance total
+    const balanceTotalElement =document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText ;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step :06 calculate new balance total
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrowAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    // step: 07 clear the withdrow field
+    withdrowField.value = ''; 
+})
