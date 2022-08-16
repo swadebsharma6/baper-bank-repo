93a47e1ad8979cf5db13cf0c964970b3490a05fc
swadebsharma6 <swadebriva2012@gmail.com>
2022-08-16 08:33:39 +0600

    added the finla

--- js/withdrow.js
+++ js/withdrow.js
@@ -14,20 +14,30 @@ document.getElementById('btn-withdrow').addEventListener('click', function(){
     const previousWithdrowTotalString = withdrowTotalElement.innerText;
     const previousWithdrowTotal = parseFloat(previousWithdrowTotalString);
 
-    // step: 04 calculate total withdrow ammount.
-    const currentWithdrowTotal = newWithdrowAmount + previousWithdrowTotal;
-    withdrowTotalElement.innerText = currentWithdrowTotal;
+    // // step: 04 calculate total withdrow ammount.
+    // const currentWithdrowTotal = newWithdrowAmount + previousWithdrowTotal;
+    // withdrowTotalElement.innerText = currentWithdrowTotal;
 
     // step: 05 get the previous balance total
     const balanceTotalElement =document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText ;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    
+    // step: 07 clear the withdrow field
+    withdrowField.value = ''; 
+    
+    if(newWithdrowAmount > previousBalanceTotal){
+        alert('You Have not enough money');
+        return;
+    }
+    // step: 04 calculate total withdrow ammount.
+    const currentWithdrowTotal = newWithdrowAmount + previousWithdrowTotal;
+    withdrowTotalElement.innerText = currentWithdrowTotal;
 
     // step :06 calculate new balance total
 
     const newBalanceTotal = previousBalanceTotal - newWithdrowAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // step: 07 clear the withdrow field
-    withdrowField.value = ''; 
+    
 })
